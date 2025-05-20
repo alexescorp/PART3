8d654dbf805227de8765a4166f5c8d76a7055400
cvquitod <dquitoch@proton.me>
2025-05-19 21:48:20 -0500

    3.6: Backend de la Agenda Telefónica, paso 6

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    if (agenda.some(contact => contact.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const persona = {
         name: body.name,
         number: body.number,
